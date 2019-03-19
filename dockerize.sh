95f54ffbf42be32fd3504a06a66648e3be8ba879
Graves, Mark <mark.graves@tylertech.com>
2019-03-19 14:51:03 -0500

    Updated logging config

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-dotnet publish -c Release
+dotnet publish -c Debug
 cd ./check-yo-self-api
-docker build . -t check-yo-self-api:1.0.0 --build-arg Configuration=Release
+docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.0 .
