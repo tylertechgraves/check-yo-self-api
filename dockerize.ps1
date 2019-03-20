364a88af201032a20b06c1aa8136b8578e4b7551
Mark Graves <mark.graves@tylertech.com>
2019-03-20 13:52:23 -0500

    Updated powershell dockerize script to match the shell script

--- dockerize.ps1
+++ dockerize.ps1
@@ -1,3 +1,4 @@
-dotnet publish -c Release -r linux-musl-x64
+dotnet publish -c Debug -r linux-musl-x64
 Set-Location ./check-yo-self-api
-docker build . -t check-yo-self-api:1.0.0 --build-arg Configuration=Release
+docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.0 .
+Set-Location ../
