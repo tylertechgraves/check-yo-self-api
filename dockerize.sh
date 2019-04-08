26e19df68b7f5709e5b61504b2fd2401d176c402
Graves, Mark <mark.graves@tylertech.com>
2019-04-08 10:39:47 -0500

    Updated dockerize.sh to have the correct version 1.0.1

--- dockerize.sh
+++ dockerize.sh
@@ -2,4 +2,4 @@
 
 dotnet publish -c Debug -r linux-musl-x64
 cd ./check-yo-self-api
-docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.0 .
+docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.1 .
