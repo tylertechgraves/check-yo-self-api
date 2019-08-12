0c2b27da11449474524bc53f60cacb4e7fd44ab9
Graves, Mark <mark.graves@tylertech.com>
2019-08-12 16:33:17 -0500

    Reordered dockerize.ps1 script

--- dockerize.ps1
+++ dockerize.ps1
@@ -1,4 +1,4 @@
-dotnet publish -c Debug -r linux-musl-x64
 Set-Location ./check-yo-self-api
-docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.1 .
+dotnet publish -c Debug -r linux-musl-x64
+docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.0 .
 Set-Location ../
