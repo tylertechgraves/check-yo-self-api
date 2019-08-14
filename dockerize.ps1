2c1b05c2bb0c2c7463f18780119aaa90d9a22aff
Graves, Mark <mark.graves@tylertech.com>
2019-08-14 13:38:29 -0500

    Updated indexer NuGet version; updated dockerize script

--- dockerize.ps1
+++ dockerize.ps1
@@ -1,4 +1,4 @@
 Set-Location ./check-yo-self-api
 dotnet publish -c Debug -r linux-musl-x64
-docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.3 .
+docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.4 .
 Set-Location ../
