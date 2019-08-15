21136c4c99f30167af9d154abceadbe709b583e0
Graves, Mark <mark.graves@tylertech.com>
2019-08-15 10:59:34 -0500

    Updated dockerize shell script to match the powershell script

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
+cd ./check-yo-self-api || exit
 dotnet publish -c Debug -r linux-musl-x64
-cd ./check-yo-self-api
-docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.1 .
+docker build --build-arg Configuration=Debug -t check-yo-self-api:1.0.4 .
+cd .. || exit
