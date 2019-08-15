b41615b5e4721b609055e56f0f759e9945636cd9
Graves, Mark <mark.graves@tylertech.com>
2019-08-15 10:59:53 -0500

    Refactored packNugets script to run from its own directory

--- check-yo-self-api-SDK/build-scripts/packNugets.ps1
+++ check-yo-self-api-SDK/build-scripts/packNugets.ps1
@@ -1,10 +1,3 @@
-Write-Host "Script was originally running in: " $(Get-Location)
-Push-Location
-Set-Location $(Join-Path $PSScriptRoot ..)
-Write-Host "Script has changed location to: " $(Get-Location)
-
-dotnet publish -c Release ../check-yo-self-api/check-yo-self-api.csproj -r linux-musl-x64
-dotnet publish -c Release ./check-yo-self-api-sdk.csproj -o ./out
-dotnet pack ./check-yo-self-api-sdk.csproj /p:PackageVersion=1.0.9-beta --configuration Debug --include-source --include-symbols --output ./nupkg --version-suffix "beta"
-
-Pop-Location
+dotnet publish ../../check-yo-self-api/check-yo-self-api.csproj -c Release -r linux-musl-x64
+dotnet publish ../check-yo-self-api-sdk.csproj -o ./out
+dotnet pack ../check-yo-self-api-sdk.csproj /p:PackageVersion=1.0.9-beta --configuration Debug --include-source --include-symbols --output ./nupkg --version-suffix "beta"
